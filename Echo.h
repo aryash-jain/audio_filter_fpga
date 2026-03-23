10b65405d88066731e5b57a7926bc91a6fbf60a1
Aryash Jain <aryash.jain@gmail.com>
2026-03-23 15:41:56 -0400

    fixing datatypes

--- Echo.h
+++ Echo.h
@@ -4,7 +4,7 @@
 #include "ap_fixed.h"
 
 #define SAMPLE_SIZE 1024
-typedef ap_fixed<24, 8> sample_t;
+typedef ap_fixed<24, 16> sample_t;
 
 void echo_control(sample_t input, sample_t output, short delay, short decay);
 
--- VolumeControl.h
+++ VolumeControl.h
@@ -4,7 +4,7 @@
 #include <ap_fixed.h>
 
 #define SAMPLE_SIZE 1024
-typedef ap_fixed<16, 4> gain_t;
+typedef ap_fixed<24, 16> gain_t;
 
 void change_volume(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], gain_t coeff);
 #endif
