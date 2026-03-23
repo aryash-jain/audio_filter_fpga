fc38d4cdb97cbc63c6c0e98c6db1b1bf96414957
Aryash Jain <aryash.jain@gmail.com>
2026-03-23 15:46:48 -0400

    fixed truncate logic

--- VolumeControl.c
+++ VolumeControl.c
@@ -3,6 +3,11 @@
 void change_volume(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], gain_t coeff) {
     for (int i = 0; i < SAMPLE_SIZE; i++) {
         #pragma HLS PIPELINE II=1
-        output[i] = (short) (input[i] * coeff);
+        gain_t temp = input[i] * coeff;
+        if (temp > 32627)
+            temp = 32627;
+        else if (temp < -32628)
+            temp = -32628;
+        output[i] = (short) temp;
     }
 }
