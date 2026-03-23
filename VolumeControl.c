bf787d040f2e277abc7d3769bffdb23b6b763a6c
Aryash Jain <aryash.jain@gmail.com>
2026-03-23 15:49:22 -0400

    pragma directives

--- VolumeControl.c
+++ VolumeControl.c
@@ -1,6 +1,10 @@
 #include "VolumeControl.h"
 
 void change_volume(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], gain_t coeff) {
+#pragma HLS INTERFACE axis port=input
+#pragma HLS INTERFACE axis port=output
+#pragma HLS INTERFACE s_axilite port=coeff
+#pragma HLS INTERFACE s_axilite port=return
     for (int i = 0; i < SAMPLE_SIZE; i++) {
         #pragma HLS PIPELINE II=1
         gain_t temp = input[i] * coeff;
