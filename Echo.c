e7edced228c6226e6feb28349e179e5e7fdccc54
Vasishta Malisetty <malisetty.v@northeastern.edu>
2026-03-25 15:08:36 -0400

    Added overflow protection for Echo algorithm

--- Echo.c
+++ Echo.c
@@ -1,14 +1,28 @@
 #include "Echo.h"
 
-void echo_control(sample_t input, sample_t output, short delay, short decay) 
+void echo_control(short input, short output, sample_t delay, sample_t decay) 
 {
+    #pragma HLS INTERFACE s_axilite port=input
+    #pragma HLS INTERFACE s_axilite port=output
+    #pragma HLS INTERFACE s_axilite port=delay
+    #pragma HLS INTERFACE s_axilite port=decay
+    #pragma HLS INTERFACE s_axilite port=return
     #pragma HLS PIPELINE II=1
     for (int i = 0; i < SAMPLE_SIZE; i++) 
     {
-        output[i] = input[i];
+        sample_t temp = input[i];
         if (i >= delay)
         {
-            output[i] += input[i - delay] * decay;
+            sum += input[i - delay] * decay;
+            if (sum > 32627)
+            {
+                sum = 32627;
+            }
+            else if (sum < -32628)
+            {
+                sum = -32628;
+            }
+            output[i] = (short) sum;
         }
     }
 }
--- Echo.h
+++ Echo.h
@@ -6,6 +6,6 @@
 #define SAMPLE_SIZE 1024
 typedef ap_fixed<24, 16> sample_t;
 
-void echo_control(sample_t input, sample_t output, short delay, short decay);
+void echo_control(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], sample_t delay, sample_t decay);
 
 #endif
