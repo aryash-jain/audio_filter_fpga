27b20aaeb3966a76bcff0d60857bddc967eb1e6c
abhinavyarlagadda <54691858+abhinavyarlagadda@users.noreply.github.com>
2026-04-01 17:17:45 -0400

    Change pitch_shift parameter type from fixed_t to short

--- PitchTrans.h
+++ PitchTrans.h
@@ -10,6 +10,6 @@
 typedef ap_fixed<16, 4> fixed_t;
 typedef ap_fixed<32, 8> accum_t;
 
-void pitch_shift(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], fixed_t tones);
+void pitch_shift(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], short tones);
 
 #endif
