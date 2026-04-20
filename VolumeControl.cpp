74ea3abbff45723404e06ec30e1b12fa2028507b
abhinavyarlagadda <54691858+abhinavyarlagadda@users.noreply.github.com>
2026-04-20 16:30:00 -0400

    Optimize volume change function with HLS pragmas
    Added HLS pragmas for input and output array partitioning.

--- VolumeControl.cpp
+++ VolumeControl.cpp
@@ -1,6 +1,8 @@
 #include "VolumeControl.h"
 
 void change_volume(short input[SAMPLE_SIZE], short output[SAMPLE_SIZE], gain_t coeff) {
+#pragma HLS ARRAY_PARTITION variable=input  cyclic factor=4 dim=1
+#pragma HLS ARRAY_PARTITION variable=output cyclic factor=4 dim=1
     for (int i = 0; i < SAMPLE_SIZE; i++) {
         #pragma HLS PIPELINE II=1
         gain_t temp = input[i] * coeff;
